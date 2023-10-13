4ebd15ba25d19624f9e094100eb2baf375975887
datalabsolution <146425301+datalabsolution@users.noreply.github.com>
2023-10-13 15:58:31 +0800

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 
-api_key = st.secrets["API_KEY"]
+
 # Function to check if a date is the third Friday of the month
 def is_third_friday(date):
     return date.weekday() == 4 and 15 <= date.day <= 21
 
 # Initialize
 today = datetime.date.today()
-client = RESTClient(api_key)
+client = RESTClient("AI73Oy1KnUYUfHAKsOJLxSmVz9dWFC95")
 option_num = 20
 
 # Streamlit UI
